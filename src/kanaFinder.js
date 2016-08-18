de9d9b08ca99ae2f34992d2a155534edf78084be
bmackey <bmackey@paycor.com>
2016-08-17 22:55:22 -0500

    Add hiragana

--- src/kanaFinder.js
+++ src/kanaFinder.js
@@ -16,7 +16,55 @@ var findHiragana = function(input) {
     case 'i': result = 'い';break;
     case 'o': result = 'お';break;
     case 'u': result = 'う';break;
+    case 'u': result = 'う';break;
+
+    case 'ka': result = 'か';break;
+    case 'ki': result = 'き';break;
+    case 'ku': result = 'く';break;
+    case 'ke': result = 'け';break;
+    case 'ko': result = 'こ';break;
+
+    case 'sa': result = 'さ';break;
+    case 'shi': result = 'し';break;
+    case 'su': result = 'す';break;
+    case 'se': result = 'せ';break;
+    case 'so': result = 'そ';break;
+
+    case 'ta': result = 'た';break;
+    case 'chi': result = 'ち';break;
+    case 'tsu': result = 'つ';break;
+    case 'te': result = 'て';break;
+    case 'to': result = 'と';break;
+
+    case 'na': result = 'な';break;
+    case 'ni': result = 'に';break;
+    case 'nu': result = 'ぬ';break;
+    case 'ne': result = 'ね';break;
+    case 'no': result = 'の';break;
+
+    case 'ha': result = 'は';break;
+    case 'hi': result = 'ひ';break;
+    case 'hu': result = 'ふ';break;
+    case 'he': result = 'へ';break;
+    case 'ho': result = 'ほ';break;
+
+    case 'ya': result = 'や';break;
+    case 'yu': result = 'ゆ';break;
+    case 'yo': result = 'よ';break;
+
+    case 'ra': result = 'ら';break;
+    case 'ri': result = 'り';break;
+    case 'ru': result = 'る';break;
+    case 're': result = 'れ';break;
+    case 'ro': result = 'ろ';break;
+
+    case 'wa': result = 'わ';break;
+    case 'wo': result = 'を';break;
+
+    case 'n': result = 'ん';break;
+
     default: result = 'unknown';break;
+
   }
 
   return result;
