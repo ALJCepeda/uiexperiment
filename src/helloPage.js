556de3316a239ce0fb83b5fedce290279d30f46f
bmackey <bmackey@paycor.com>
2016-08-17 20:09:24 -0500

    Add basic functionality

--- src/helloPage.js
+++ src/helloPage.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import KanaFinder from './kanaFinder'
 
 var Hello = React.createClass({
+  getInitialState: function() {
+    return {
+      output: '',
+      hiragana: true,
+      katakana: false,
+      kanji: false,
+      input: '',
+    };
+  },
+
+  getKanaSystem(){
+    if(this.state.hiragana){
+      return 'hiragana';
+    }else if(this.state.katakana){
+      return 'katakana';
+    }else{
+      return 'kanji'
+    }
+  },
+
+  onChange(event){
+    var input = event.target.value;
+    this.setState({input: input});
+    console.log('input: ' + input);
+    this.findKana(input, this.getKanaSystem());
+  },
+
+  findKana(input, activeKana){
+    var result = KanaFinder.findKana(activeKana, input);
+    console.log('result: ' + result);
+    this.setState({output: result});
+  },
+
+  toggleKana(event){
+    var activeKana = event.target.value;
+
+    if('katakana' === activeKana){
+      this.setState({hiragana: false});
+      this.setState({katakana: true});
+      this.setState({kanji: false});
+    }else if('hiragana' ===activeKana){
+      this.setState({hiragana: true});
+      this.setState({katakana: false});
+      this.setState({kanji: false});
+    }else{
+      this.setState({hiragana: false});
+      this.setState({katakana: false});
+      this.setState({kanji: true});
+    }
+
+    this.findKana(this.state.input, activeKana);
+  },
+
   render: function() {
     return (
         <div>
-          <input id="myInput" type="text" />
-          <h2>Nothing to see here folks</h2>
-          <div id="someId">Some content</div>
+          <span>Japanese</span>
+          <input id="myInput" type="text" onChange={this.onChange} value={this.state.input} />
+          <div>{this.state.output}</div>
+          <input onChange={this.toggleKana} type="radio" name="kana" value="hiragana" checked={this.state.hiragana} /><span>Hiragana</span><br />
+          <input onChange={this.toggleKana} type="radio" name="kana" value="katakana" /><span>Katakana</span><br />
+          <input onChange={this.toggleKana} type="radio" name="kana" value="kanji" /><span>Kanji</span><br />
         </div>
     );
   }
---
+++ src/kanaFinder.js
@@ -0,0 +1,61 @@
+var internalMethod = function() {
+    return 'hi';
+};
+
+var internalMethod2 = function() {
+    return 'bye';
+};
+
+var findHiragana = function(input) {
+  input = input.toLowerCase();
+  var result;
+
+  switch(input){
+    case 'a': result = 'あ';break;
+    default: result = 'unknown';break;
+  }
+
+  return result;
+};
+
+var findKatakana = function(input) {
+  input = input.toLowerCase();
+  var result;
+
+  switch(input){
+    case 'a': result = 'ア';break;
+    default: result = 'unknown';break;
+  }
+
+  return result;
+};
+
+var findKanji = function(input) {
+  input = input.toLowerCase();
+  var result;
+
+  switch(input){
+    case 'a': result = 'KANJI';break;
+    default: result = 'unknown';break;
+  }
+
+  return result;
+};
+
+var findKana = function(kana, input) {
+    if(kana === 'hiragana'){
+      return findHiragana(input);
+    }else if(kana === 'katakana'){
+      return findKatakana(input);
+    }else{
+      return findKanji(input);
+    }
+};
+
+var KanaFinder = {
+      'func1': internalMethod(),
+      'func2': internalMethod2(),
+      'findKana': findKana
+};
+
+module.exports = KanaFinder;
